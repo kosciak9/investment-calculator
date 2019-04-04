1434c24a9f873a4d4954b94364b09a484a60cadd
Franciszek Madej <f.madej@protonmail.com>
2019-04-04 18:28:03 +0200

    Add basic layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <section className="investment column">
+          <div className="price form-part">
+            <input type="range" />
+            price:
+            <input type="number" />
+          </div>
+          <div className="amount form-part">
+            <input type="range" />
+            amount:
+            <input type="number" />
+          </div>
+          <div className="token form-part">
+            token:
+            <input type="select" />
+          </div>
+        </section>
+        <aside className="earnings column">
+          <h1>earnings:</h1>
+          <div class="usd">$1234.56</div>
+          <div class="btc">12.56 BTC</div>
+          <div class="eth">123.56 ETH</div>
+          <div class="xrp">12345.78 XRP</div>
+        </aside>
       </div>
     );
   }
